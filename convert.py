d6298a9c99bb46134eeb78e1fea9db8dc2a60519
Jane Ann Ilagan <janeannilagan@outlook.com>
2023-05-26 17:41:17 +0800

    review changes and update diff

--- convert.py
+++ convert.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import re
 from typing import Dict, List, Tuple
 
@@ -12,6 +13,11 @@ from utils import (
     write_settings,
 )
 
+
+def sortFiles(path: Path):
+    posix_path = path.as_posix().casefold()
+    return ("Home.md" not in posix_path, posix_path[:posix_path.rfind("/")], path.name.casefold())
+
 if __name__ == "__main__":
 
     Settings.parse_env()
@@ -24,9 +30,10 @@ if __name__ == "__main__":
     edges: List[Tuple[str, str]] = []
     section_count = 0
 
-    all_paths = list(sorted(raw_dir.glob("**/*")))
+    all_paths = [raw_dir, *list(sorted(raw_dir.glob("**/*"), key=sortFiles))]
 
-    for path in [raw_dir, *all_paths]:
+    for i in range(0, len(all_paths)):
+        path = all_paths[i]
         doc_path = DocPath(path)
         if doc_path.is_file:
             if doc_path.is_md:
@@ -43,6 +50,39 @@ if __name__ == "__main__":
                     parsed_lines.append(parsed_line)
 
                     edges.extend([doc_path.edge(rel_path) for rel_path in linked])
+                    
+                previous_md = {
+                    'title': None,
+                    'path': None
+                }
+                next_md = {
+                    'title': None,
+                    'path': None
+                }
+                
+                if i > 0:
+                    goBack = 1
+                    previous_doc_path = DocPath(all_paths[i - goBack])
+                    while not previous_doc_path.is_md:
+                        goBack += 1
+                        previous_doc_path = DocPath(all_paths[i - goBack])
+                    previous_md = {
+                        "path": previous_doc_path.abs_url,
+                        "title": previous_doc_path.page_title 
+                    }
+                
+                if i < len(all_paths) - 1:
+                    goNext = 1
+                    next_doc_path = DocPath(all_paths[i + goNext])
+                    while not next_doc_path.is_md:
+                        goNext += 1
+                        next_doc_path = DocPath(all_paths[i + goNext])
+                    next_md = {
+                        "path": next_doc_path.abs_url,
+                        "title": next_doc_path.page_title
+                    }
+                    
+                section_count += 1
 
                 content = [
                     "---",
@@ -50,12 +90,20 @@ if __name__ == "__main__":
                     f"date: {doc_path.modified}",
                     f"updated: {doc_path.modified}",
                     "template: docs/page.html",
+                    f"weight: {section_count}",
+                    "extra:",
+                    f"    previous:",
+                    f"         title: {previous_md['title']}",
+                    f"         path: {previous_md['path']}",
+                    f"    next:",
+                    f"         title: {next_md['title']}",
+                    f"         path: {next_md['path']}",
                     "---",
                     # To add last line-break
                     "",
                 ]
                 doc_path.write(["\n".join(content), *parsed_lines])
-                print(f"Found page: {doc_path.new_rel_path}")
+                print(f"Found page: {doc_path.new_rel_path}, doc_path.abs: {doc_path.abs_url}, path: {path}")
             else:
                 # Resource
                 doc_path.copy()
@@ -76,7 +124,7 @@ if __name__ == "__main__":
                 # To add last line-break
                 "",
             ]
-            section_count += 1
+            section_count += 1000 # Why? allow 1000 subitems/items inside a section
             doc_path.write_to("_index.md", "\n".join(content))
             print(f"Found section: {doc_path.new_rel_path}")
 
